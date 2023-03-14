d6ab1163ac0555e2e275583d28ebac5f10e8db5c
conkobar <connorkleine@gmail.com>
2023-03-14 14:53:23 -0500

    task 4

---
+++ 0x00-Javascript_advanced/4-math.js
@@ -0,0 +1,19 @@
+function divideBy(firstNumber) {
+  return (secondNumber) => secondNumber / firstNumber;
+}
+
+function addBy(firstNumber) {
+  return (secondNumber) => firstNumber + secondNumber;
+}
+
+// create some closures
+const addBy100 = addBy(100);
+const addBy1000 = addBy(1000);
+const divideBy10 = divideBy(10);
+const divideBy100 = divideBy(100);
+
+// examples
+console.log(addBy100(20));
+console.log(divideBy10(20));
+console.log(divideBy100(200));
+console.log(addBy1000(20));
