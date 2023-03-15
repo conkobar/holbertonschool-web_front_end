3759c1e4ee4b8b82b3db029807fb12e1fe6a025c
conkobar <connorkleine@gmail.com>
2023-03-15 10:13:03 -0500

    task 8

---
+++ 0x00-Javascript_advanced/8-payments.js
@@ -0,0 +1,19 @@
+function processPayment(amount) {
+  console.log(`Collecting payment of ${amount}`);
+}
+
+function processOrder(orderId, amount) {
+  console.log(`${orderId} is being processed`);
+  processPayment(amount);
+  console.log(`${orderId} has been fully processed`);
+}
+
+console.log('Processing orders');
+
+// examples
+processOrder(12321, 10.99);
+processOrder(12322, 12.99);
+processOrder(12323, 15.0);
+
+// log that it is finished
+console.log('All the orders have been processed');
